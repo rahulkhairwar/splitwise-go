0b5c7fd1eb1d23e3a7d19a0e171dd87f761334d5
Rahul Khairwar <rahulkhairwar@gmail.com>
2020-11-10 02:05:19 +0800

    - Implemented UpdateUser API (#5)
    - Minor changes to the User dto

--- splitwise/client.go
+++ splitwise/client.go
@@ -14,6 +14,8 @@ import (
 
 var alphabet = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ+-0123456789")
 
+// Client struct stores configuration for Splitwise oAuth2.0 login, and also the access token, once it's been fetched
+// after the user has granted access to their account.
 type Client struct {
 	*RestClient
 	conf           *oauth2.Config
@@ -23,6 +25,7 @@ type Client struct {
 	accessToken    string
 }
 
+// New creates and returns a new Splitwise Client.
 func New(consumerKey, secret, redirectUrl, errRedirectUrl string, httpClient http.Client) *Client {
 	return &Client{
 		RestClient: &RestClient{
@@ -42,15 +45,30 @@ func New(consumerKey, secret, redirectUrl, errRedirectUrl string, httpClient htt
 	}
 }
 
+func (c *Client) GetState() string {
+	return c.state
+}
+
+func (c* Client) SetAccessToken(a string) {
+	c.accessToken = a
+}
+
+// HandleLogin is an HTTP handler to connect to Splitwise via oAuth2 flow, redirecting to the Splitwise authorization
+// page, for the user to log in, and grant API access to their data. This handler also generates a random state,
+// as required by the oAuth2.0 flow, and sends it along with the request.
 func (c *Client) HandleLogin(w http.ResponseWriter, r *http.Request) {
 	c.state = generateRandomState()
 	url := c.conf.AuthCodeURL(c.state)
 	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
 }
 
+// HandleCallback is an HTTP handler to handle the callback from Splitwise, after authentication by the user. This
+// handler can be replaced by your own implementation of a handler easily. Use the client.GetState() function to check
+// the stored state, and store the returned access token using client.SetToken().
 func (c *Client) HandleCallback(w http.ResponseWriter, r *http.Request) {
 	state := r.FormValue("state")
 	if state == "" || state != c.state {
+		// todo : should (level-)log this, to the logger provided by the user.
 		fmt.Println("Invalid state!")
 		http.Redirect(w, r, c.errRedirectUrl, http.StatusTemporaryRedirect)
 		return
@@ -65,8 +83,32 @@ func (c *Client) HandleCallback(w http.ResponseWriter, r *http.Request) {
 	c.accessToken = token.AccessToken
 }
 
+// GetCurrentUser API allows a user to fetch their account information. All fields of the user's account are returned.
 func (c *Client) GetCurrentUser(ctx context.Context) (*User, error) {
 	url := c.addAccessTokenToUrl(GetCurrentUserUrl)
+	r, err := c.Get(ctx, url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var resp struct {
+		User *User `json:"user"`
+	}
+	bts, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println("resp.body : ", string(bts))
+	if err = json.Unmarshal(bts, &resp); err != nil {
+		return nil, err
+	}
+	return resp.User, err
+}
+
+// GetUser API allows a user to fetch another user's information. For the non-active user, all fields will not be
+// returned.
+func (c *Client) GetUser(ctx context.Context, id int64) (*User, error) {
+	url := c.addAccessTokenToUrl(fmt.Sprintf(GetUserByIdUrl, id))
 	r, err := c.Get(ctx, url, nil)
 	if err != nil {
 		return nil, err
@@ -85,24 +127,28 @@ func (c *Client) GetCurrentUser(ctx context.Context) (*User, error) {
 	return resp.User, err
 }
 
-func (c* Client) GetUser(ctx context.Context, id int64) (*User, error) {
-	url := c.addAccessTokenToUrl(fmt.Sprintf(GetUserUrl, id))
-	r, err := c.Get(ctx, url, nil)
-	if err != nil {
-		return nil, err
-	}
+// UpdateUser allows one to update information about their own account, and allows editing of the first_name, last_name,
+// and email for acquaintances who have not logged in yet.
+func (c *Client) UpdateUser(ctx context.Context, user *User) (*UpdateUserResponse, error) {
+	url := c.addAccessTokenToUrl(fmt.Sprintf(UpdateUserByIdUrl, user.Id))
 
-	var resp struct {
-		User *User `json:"user"`
-	}
-	bts, err := ioutil.ReadAll(r.Body)
+	type alias *User
+	req := (alias)(user)
+	req.NotificationSettings = user.NotificationSettings
+
+	resp, err := c.Post(ctx, url, req, nil)
 	if err != nil {
 		return nil, err
 	}
-	if err = json.Unmarshal(bts, &resp); err != nil {
+	bts, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
 		return nil, err
 	}
-	return resp.User, err
+	var r UpdateUserResponse
+	if err = json.Unmarshal(bts, &r); err != nil {
+		return nil, err
+	}
+	return &r, nil
 }
 
 func (c *Client) addAccessTokenToUrl(url string) string {
--- splitwise/responses.go
+++ splitwise/responses.go
@@ -1,5 +1,11 @@
 package splitwise
 
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/kr/pretty"
+)
+
 type User struct {
 	Id        int    `json:"id"`
 	FirstName string `json:"first_name"`
@@ -9,13 +15,86 @@ type User struct {
 		Medium string `json:"medium,omitempty"`
 		Large  string `json:"large,omitempty"`
 	} `json:"picture,omitempty"`
-	Email                    string `json:"email,omitempty"`
-	RegistrationStatus       string `json:"registration_status,omitempty"`
-	DefaultCurrency          string `json:"default_currency,omitempty"`
-	Locale                   string `json:"locale,omitempty"`
-	NotificationsRead        string `json:"notifications_read,omitempty"`
-	NotificationsCount       int    `json:"notifications_count,omitempty"`
-	NotificationsPreferences *struct {
-		AddedAsFriend bool `json:"added_as_friend,omitempty"`
-	} `json:"notifications,omitempty"`
+	Email                string                `json:"email,omitempty"`
+	Password             string                `json:"password,omitempty"`
+	RegistrationStatus   string                `json:"registration_status,omitempty"`
+	DateFormat           string                `json:"date_format,omitempty"`
+	DefaultCurrency      string                `json:"default_currency,omitempty"`
+	DefaultGroupId       int64                 `json:"default_group_id,omitempty"`
+	Locale               string                `json:"locale,omitempty"`
+	NotificationsRead    string                `json:"notifications_read,omitempty"`
+	NotificationsCount   int                   `json:"notifications_count,omitempty"`
+	NotificationSettings *NotificationSettings `json:"notifications,omitempty"`
+	// NotificationSettings     *NotificationSettings `json:"notification_settings,omitempty"`
+}
+
+func (u *User) String() string {
+	return fmt.Sprintf("%# v", pretty.Formatter(u))
+}
+
+/*func (u *User) MarshalJSON() ([]byte, error) {
+	// n := u.NotificationSettings
+	return nil, nil
+}*/
+
+/*func (u *User) UnmarshalJSON(data []byte) error {
+	// if err := json.Unmarshal(data, u); err != nil {
+	// 	return err
+	// }
+
+	var m map[string]interface{}
+	if err := json.Unmarshal(data, &m); err != nil {
+		return err
+	}
+
+	if m["notification_settings"] != nil {
+		u.NotificationSettings = m["notification_settings"].(*NotificationSettings)
+	} else if m["notifications"] != nil {
+		u.NotificationSettings = m["notifications"].(*NotificationSettings)
+	}
+
+	fmt.Printf("m : %+v\nu : %+v\n", m, u)
+	return nil
+}*/
+
+// type UpdateUser struct {
+// 	FirstName            string                `json:"first_name"`
+// 	LastName             string                `json:"last_name"`
+// 	Email                string                `json:"email,omitempty"`
+// 	Password             string                `json:"password,omitempty"`
+// 	DateFormat           string                `json:"date_format,omitempty"`
+// 	DefaultCurrency      string                `json:"default_currency,omitempty"`
+// 	DefaultGroupId       string                `json:"default_group_id,omitempty"`
+// 	Locale               string                `json:"locale,omitempty"`
+// 	NotificationSettings *NotificationSettings `json:"notification_settings,omitempty"`
+// }
+
+type NotificationSettings struct {
+	AddedAsFriend  bool `json:"added_as_friend,omitempty"`
+	AddedToGroup   bool `json:"added_to_group,omitempty"`
+	ExpenseAdded   bool `json:"expense_added,omitempty"`
+	ExpenseUpdated bool `json:"expense_updated,omitempty"`
+	Bills          bool `json:"bills,omitempty"`
+	Payments       bool `json:"payments,omitempty"`
+	MonthlySummary bool `json:"monthly_summary,omitempty"`
+	Announcements  bool `json:"announcements,omitempty"`
+}
+
+func (n *NotificationSettings) String() string {
+	return fmt.Sprintf("%# v", pretty.Formatter(n))
+	// return indentString(n)
+	// return fmt.Sprintf("{ AddedAsFriend : %+v, AddedToGroup : %+v, ExpenseAdded : %+v, ExpenseUpdated : %+v, Bills : %+v, Payments : %+v, MonthlySummary : %+v, Announcements : %+v }\n")
+}
+
+type UpdateUserResponse struct {
+	User *User `json:"user"`
+	Errors []interface{} `json:"errors"`
+}
+
+func indentString(v interface{}) (string, error) {
+	b, err := json.MarshalIndent(v, "", "  ")
+	if err != nil {
+		return "", err
+	}
+	return string(b), nil
 }
